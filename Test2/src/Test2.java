1eb9e21429b552b08e6e92d81a6a9f78f4fa034f
Hmachhuana <Hmachhuana@192.168.1.208>
2018-08-29 18:05:05 +0530

    Test2

---
+++ Test2/src/Test2.java
@@ -0,0 +1,20 @@
+//SWITCH CASE
+public class Test2 {
+
+	public static void main(String[] args) {
+		int i=1;
+		switch(i)
+		{
+		case 1:
+			System.out.print("A ");
+		case 2:
+			System.out.print("B ");
+		case 3:
+			System.out.print("C ");
+			default:
+				System.out.print("\nA B C");
+		}
+
+	}
+
+}
